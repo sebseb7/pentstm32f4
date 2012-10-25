7e49499340a579e13d4cc70e6abe32439a510355
sebseb7 <github@seb.exse.net>
2012-10-25 07:28:37 +0200

    exposed i2c internals

--- lib/i2c.h
+++ lib/i2c.h
@@ -7,4 +7,9 @@ void i2cWrite(uint8_t addr,uint8_t reg, uint8_t byte);
 int16_t i2cRead16s(uint8_t addr,uint8_t reg);
 int16_t i2cRead16sX(uint8_t addr,uint8_t reg);
 
+void I2C_start(I2C_TypeDef* I2Cx, uint8_t address, uint8_t direction);
+void I2C_write(I2C_TypeDef* I2Cx, uint8_t data);
+uint8_t I2C_read_ack(I2C_TypeDef* I2Cx);
+uint8_t I2C_read_nack(I2C_TypeDef* I2Cx);
+void I2C_stop(I2C_TypeDef* I2Cx);
 #endif
